7e2501684c6420fd99f52526e3bf293587ca2d54
ibrahim ali <ibrahim@ibrahim-laptop.(none)>
2009-07-02 19:59:12 +0300

    commented the unused code and added a method to get the channel URLs in order to load the channel list from the db instead of the feedstore

--- dbmanager.cpp
+++ dbmanager.cpp
@@ -1,26 +1,37 @@
 #include "dbmanager.h"
-
+#include <QStringList>
+#include "dbconnector.h"
+#include <QSqlQuery>
+#include <QVariant>
 DBManager::DBManager()
 {
     //init member to NULL
-    m_pChannel = NULL;
+    /*m_pChannel = NULL;
+    m_pItem = NULL;*/
     m_pConnector = NULL;
-    m_pItem = NULL;
+}
+DBManager::DBManager(QObject* parent):QObject(parent)
+{
+    //init member to NULL
+    /*m_pChannel = NULL;
+    m_pItem = NULL;*/
+    m_pConnector = NULL;
 }
 DBManager::~DBManager()
 {
+    qDebug("in dbmanager's destructor");
     CleanUp();
 }
 void DBManager::CleanUp()
 {
-    if (m_pItem){
+    /*if (m_pItem){
         delete m_pItem;
         m_pItem = NULL;
     }
     if (m_pChannel){
         delete m_pChannel;
         m_pChannel = NULL;
-    }
+    }*/
     if (m_pConnector){
         delete m_pConnector;
         m_pConnector = NULL;
@@ -29,6 +40,41 @@ void DBManager::CleanUp()
 /**
   this fn retreives ALL the RSS items from the DB
   */
-void DBManager::GetAllData()
+/*void DBManager::GetAllData()
 {
+}*/
+
+QStringList DBManager::GetChannelsURLs()
+{ 
+    qDebug("in GetChannelsURLs(), dbmanager");
+    QStringList channelsList;
+    if (! m_pConnector){
+        m_pConnector = new DBConnector(this);
+    }
+    QString channelsURLsQuery = "select channel_link from channel_table";
+    QSqlQuery result = m_pConnector->RetreiveData(channelsURLsQuery);
+    if ( ! result.isValid() ||
+            !result.isActive() || !result.first()){
+        //the query result is null , damn it
+        qDebug("the query result is null , i am in DBManager");
+        //return channelsList; // which is an empty list
+    }
+    //else, the query result is OK
+    while (result.next()){
+        channelsList.append(result.value(0).toString());
+    }
+    qDebug("the size of the list: %d",channelsList.size());
+    return channelsList;
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- dbmanager.h
+++ dbmanager.h
@@ -2,8 +2,8 @@
 #define DBMANAGER_H
 
 #include <QObject>
-class Channel;
-class Item;
+/*class Channel;
+class Item;*/
 class DBConnector;
 /**
   This Class is the one responsible for reading/Writing to/from
@@ -14,20 +14,22 @@ class DBManager : public QObject
     Q_OBJECT
 public:
     DBManager();
+    DBManager(QObject* parent);
     virtual ~DBManager();
-    void GetTags();
+    /*void GetTags();
     void GetAllData();
     void GetUnread();
     void GetRecent();
     void StoreChannel(Channel&);
-    void StoreItem(Item&);
+    void StoreItem(Item&);*/
+    QStringList GetChannelsURLs();
 
 private:
     void CleanUp();
 
 private:
-    Channel*        m_pChannel;
-    Item*           m_pItem;
+    /*Channel*        m_pChannel;
+    Item*           m_pItem;*/
     DBConnector*    m_pConnector;
 };
 
